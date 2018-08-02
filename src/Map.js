20fd6e337eba8365b3bda952226c1bf66b038b0b
Andreas Thaler <andi@hoehenluft.com>
2018-08-02 16:01:24 +0200

    Added Eventlisteners to Click on Marker and Click on Map. Added InfoWindows.

--- src/Map.js
+++ src/Map.js
@@ -4,15 +4,34 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'
 
 
 export class MapContainer extends Component {
+
+  state = {
+  showingInfoWindow: false,
+  activeMarker: {},
+  selectedPlace: {},
+  };
+
+  onMarkerClick = (props, marker, e) => {
+        this.setState({
+          selectedPlace: props,
+          activeMarker: marker,
+          showingInfoWindow: true
+        })
+  }
+
+  onMapClicked = (props) => {
+     if (this.state.showingInfoWindow) {
+       this.setState({
+         showingInfoWindow: false,
+         activeMarker: null,
+         selectedPlace: [],
+       })
+     }
+   }
+
   render() {
 
-
-
-    var fireIcon = changeMarkerColor('ff0000')
-    var ambuIcon = changeMarkerColor('ffffff')
-    var poliIcon = changeMarkerColor('0000ff')
-
-    function changeMarkerColor(markerColor) {
+    const changeMarkerColor = markerColor => {
       var markerImage = new google.maps.MarkerImage(
         'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
         '|40|_|%E2%80%A2',
@@ -22,11 +41,20 @@ export class MapContainer extends Component {
         new google.maps.Size(21, 34));
         return markerImage;
     }
+
+    var fireIcon = changeMarkerColor('ff0000')
+    var ambuIcon = changeMarkerColor('ffffff')
+    // eslint-disable-next-line
+    var poliIcon = changeMarkerColor('0000ff')
+
+
+
+
     var locations = [
     {title: 'Fire Brigade Innsbruck', icon: fireIcon, position: { lat: 47.2608639, lng: 11.4051108 }},
     {title: 'Red Cross Innsbruck', icon: ambuIcon, position: { lat: 47.2600461, lng: 11.4046938 }},
     {title: 'Tyrolean Air Ambulance', icon: ambuIcon, position: { lat: 47.2576489, lng: 11.3513075 }},
-    {title: 'Public County & University Hospital Innsbruck', icon: ambuIcon, position: { lat: 47.2576489, lng: 11.3513075 }},
+    {title: 'Public County- & University Hospital Innsbruck', icon: ambuIcon, position: { lat: 47.2632716, lng: 11.3877864 }},
     {title: 'Volunteer Fire Department Hötting', icon: fireIcon, position: { lat: 47.2722737, lng: 11.385355 }},
     {title: 'Volunteer Fire Department Hungerburg', icon: fireIcon, position: { lat: 47.2863071, lng: 11.3956279 }},
     {title: 'Volunteer Fire Department Mühlau', icon: fireIcon, position: { lat: 47.2809358, lng: 11.4072096 }},
@@ -37,9 +65,8 @@ export class MapContainer extends Component {
 
     var bounds = new this.props.google.maps.LatLngBounds();
     for (var i = 0; i < locations.length; i++) {
-      bounds.extend(locations[i].position);
-    };
-
+      bounds.extend(locations[i].position)
+    }
 
     return (
       <Map
@@ -50,25 +77,29 @@ export class MapContainer extends Component {
         lng: 11.4144684
       }}
       bounds={bounds}
-      zoom={13}>
+      zoom={13}
+      onClick= {this.onMapClicked}>
 
       {locations.map((location, i) => {
         return <Marker
                   title={locations[i].title}
                   position={locations[i].position}
                   icon= {locations[i].icon}
-                  animation= {google.maps.Animation.DROP}
                   key = {locations[i].title}
+                  animation={(this.state.selectedPlace.title === location.title)
+                    && this.props.google.maps.Animation.BOUNCE}
+                  onClick= {this.onMarkerClick}
                 />
       })}
 
 
-
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>Hello There</h1>
-            </div>
-        </InfoWindow>
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+          <div>
+            <h1>{this.state.selectedPlace.title}</h1>
+          </div>
+          </InfoWindow>
       </Map>
     );
   }
