6fc400bf0a8cbe542a281242b779e29ee3112443
Andreas Thaler <andi@hoehenluft.com>
2018-08-03 17:01:08 +0200

    Added Headline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import MapContainer from './Map.js'
+import Headline from './Headline.js'
 
 class App extends Component {
   render() {
     return (
         <div style={{height: '100%', width: '100%'}}>
+        <Headline/>
         <MapContainer/>
 
         </div>
---
+++ src/Headline.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import YouTube from 'react-youtube'
+
+class Headline extends React.Component {
+
+  render() {
+
+    return (
+      <div id="head">
+      <h1 id="head-text">Emergency Services Innsbruck</h1>
+      </div>
+
+  )
+}
+}
+
+export default Headline
