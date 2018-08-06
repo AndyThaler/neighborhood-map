8f4bee29e692a0dbc96ad8eccf5844fc8e69394d
Andreas Thaler <andi@hoehenluft.com>
2018-08-06 11:14:11 +0200

    Added description to locations and changed text layout in list-items

--- src/ListOrg.js
+++ src/ListOrg.js
@@ -16,16 +16,16 @@ class ListOrg extends React.Component {
 
 
     var locations = [
-    {title: 'Fire Brigade Innsbruck', iconstate: 'fireIcon', position: { lat: 47.2608639, lng: 11.4051108 }},
-    {title: 'Red Cross Innsbruck', iconstate: 'ambuIcon', position: { lat: 47.2600461, lng: 11.4046938 }},
-    {title: 'Tyrolean Air Ambulance', iconstate: 'ambuIcon', position: { lat: 47.2576489, lng: 11.3513075 }},
-    {title: 'Public County- & University Hospital Innsbruck', iconstate: 'ambuIcon', position: { lat: 47.2632716, lng: 11.3877864 }},
-    {title: 'Volunteer Fire Department Hötting', iconstate: 'fireIcon', position: { lat: 47.2722737, lng: 11.385355 }},
-    {title: 'Volunteer Fire Department Hungerburg', iconstate: 'fireIcon', position: { lat: 47.2863071, lng: 11.3956279 }},
-    {title: 'Volunteer Fire Department Mühlau', iconstate: 'fireIcon', position: { lat: 47.2809358, lng: 11.4072096 }},
-    {title: 'Volunteer Fire Department Reichenau', iconstate: 'fireIcon', position: { lat: 47.2724776, lng: 11.4310706 }},
-    {title: 'Volunteer Fire Department Arzl', iconstate: 'fireIcon', position: { lat: 47.2840728, lng: 11.4332432 }},
-    {title: 'Volunteer Fire Department New Arzl', iconstate: 'fireIcon', position: { lat: 47.2738388, lng: 11.4444762 }}
+    {title: 'Fire Brigade Innsbruck', description: 'The only paid fire brigade in the county, that does not belong to a company.', iconstate: 'fireIcon', position: { lat: 47.2608639, lng: 11.4051108 }},
+    {title: 'Red Cross Innsbruck', description: 'The biggest ambulance station in the whole county.', iconstate: 'ambuIcon', position: { lat: 47.2600461, lng: 11.4046938 }},
+    {title: 'Tyrolean Air Ambulance', description: 'A tyrolean service, to bring heavily injured back home.', iconstate: 'ambuIcon', position: { lat: 47.2576489, lng: 11.3513075 }},
+    {title: 'Public County- & University Hospital Innsbruck', description: 'The biggest hospital in the county, with worldwide known doctors.', iconstate: 'ambuIcon', position: { lat: 47.2632716, lng: 11.3877864 }},
+    {title: 'Volunteer Fire Department Hötting', description: 'The voluntary fire outpost in Hötting.', iconstate: 'fireIcon', position: { lat: 47.2722737, lng: 11.385355 }},
+    {title: 'Volunteer Fire Department Hungerburg', description: 'The voluntary fire outpost in Hungerburg.', iconstate: 'fireIcon', position: { lat: 47.2863071, lng: 11.3956279 }},
+    {title: 'Volunteer Fire Department Mühlau', description: 'The voluntary fire outpost in Mühlau.', iconstate: 'fireIcon', position: { lat: 47.2809358, lng: 11.4072096 }},
+    {title: 'Volunteer Fire Department Reichenau', description: 'The voluntary fire outpost in Reichenau.', iconstate: 'fireIcon', position: { lat: 47.2724776, lng: 11.4310706 }},
+    {title: 'Volunteer Fire Department Arzl', description: 'The voluntary fire outpost in Arzl.', iconstate: 'fireIcon', position: { lat: 47.2840728, lng: 11.4332432 }},
+    {title: 'Volunteer Fire Department New Arzl', description: 'The voluntary fire outpost in New Arzl.', iconstate: 'fireIcon', position: { lat: 47.2738388, lng: 11.4444762 }}
    ];
 
    let showingServices
@@ -53,7 +53,7 @@ class ListOrg extends React.Component {
                 <h3>{service.title}</h3>
           </div>
           <div className="service-description">
-                <p>{service.iconstate}</p>
+                <p>{service.description}</p>
           </div>
           </li>
           ))}
