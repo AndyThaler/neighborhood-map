dc6c99169db1625670ae4708fb4761353bdbb5fe
Andreas Thaler <andi@hoehenluft.com>
2018-08-16 13:54:23 +0200

    Changed description of location

--- src/ListOrg.js
+++ src/ListOrg.js
@@ -37,10 +37,10 @@ class ListOrg extends React.Component {
 
     //the different locations
     var locations = [
-    {title: 'Fire Brigade Innsbruck', category: 'Fire', description: 'The only paid fire brigade in the county, that does not belong to a company.', iconstate: 'fireIcon', position: { lat: 47.2608639, lng: 11.4051108 }},
+    {title: 'Fire Brigade Innsbruck', category: 'Fire', description: 'The only paid fire brigade in the county.', iconstate: 'fireIcon', position: { lat: 47.2608639, lng: 11.4051108 }},
     {title: 'Red Cross Innsbruck', category: 'Ambulance', description: 'The biggest ambulance station in the whole county.', iconstate: 'ambuIcon', position: { lat: 47.2600461, lng: 11.4046938 }},
     {title: 'Tyrolean Air Ambulance', category: 'Plane', description: 'A tyrolean service, to bring heavily injured back home.', iconstate: 'ambuIcon', position: { lat: 47.2576489, lng: 11.3513075 }},
-    {title: 'Public County- & University Hospital Innsbruck', category: 'Hospital', description: 'The biggest hospital in the county, with worldwide known doctors.', iconstate: 'ambuIcon', position: { lat: 47.2632716, lng: 11.3877864 }},
+    {title: 'Public County- & University Hospital Innsbruck', category: 'Hospital', description: 'The biggest hospital in the county.', iconstate: 'ambuIcon', position: { lat: 47.2632716, lng: 11.3877864 }},
     {title: 'Volunteer Fire Department Hötting', category: 'Fire', description: 'The voluntary fire outpost in Hötting.', iconstate: 'fireIcon', position: { lat: 47.2722737, lng: 11.385355 }},
     {title: 'Volunteer Fire Department Hungerburg', category: 'Fire', description: 'The voluntary fire outpost in Hungerburg.', iconstate: 'fireIcon', position: { lat: 47.2863071, lng: 11.3956279 }},
     {title: 'Volunteer Fire Department Mühlau', category: 'Fire', description: 'The voluntary fire outpost in Mühlau.', iconstate: 'fireIcon', position: { lat: 47.2809358, lng: 11.4072096 }},
